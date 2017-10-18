9b0141a6c4d4b4009c470bd670ecbeafd943c9db
Shrayas <shrayasr@gmail.com>
2017-10-18 18:23:31 +0530

    Implement `--url`

--- jsam/Program.cs
+++ jsam/Program.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net.Http;
 
 namespace jsam
 {
@@ -13,11 +14,11 @@ namespace jsam
         [Option('d', "depth", Required = false,
             HelpText = "If arrays are discovered, how many elements to take?")]
         public int? Depth { get; set; }
+        */
 
         [Option('u', "url", Required = false,
-            HelpText = "The URL to fetch the json from")]
-        public string URL { get; set; }
-        */
+            HelpText = "Treat the argument as a URL")]
+        public bool URL { get; set; }
 
         [Option("indent-output", Required = false,
             HelpText = "Indent the output?")]
@@ -34,58 +35,83 @@ namespace jsam
 
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            var result = Parser
+            var exitCode = Parser
                             .Default
                             .ParseArguments<Options>(args)
-                            .WithParsed(options => RunAndExit(options));
+                            .MapResult(
+                                options => RunAndExit(options),
+                                _ => (int)ExitCodes.ImproperArguments
+                            );
 
-            Console.ReadKey();
+            //Console.ReadKey();
+
+            return exitCode;
         }
 
         private static int RunAndExit(Options options)
         {
-            var filePath = options.Path;
-            var indentOutput = options.IndentOutput;
-            var time = options.Time;
-
-            /*
-            var depth = options.Depth;
-            var hasDepth = depth.HasValue;
-
-            var url = options.URL;
-            var hasURL = (url != null);
-            */
-
-            if (!File.Exists(filePath))
+            try
             {
-                Console.Error.WriteLine($"No file found at '{filePath}'");
-                return 1;
+                var path = options.Path;
+                var indentOutput = options.IndentOutput;
+                var time = options.Time;
+                var url = options.URL;
+
+                /*
+                var depth = options.Depth;
+                var hasDepth = depth.HasValue;
+                */
+
+                var content = "";
+
+                if (!url)
+                {
+                    if (!File.Exists(path))
+                    {
+                        Console.Error.WriteLine($"No file found at '{path}'");
+                        return 1;
+                    }
+
+                    content = File.ReadAllText(path);
+                }
+                else
+                {
+                    using (var httpClient = new HttpClient())
+                    {
+                        var response = httpClient.GetAsync(path).Result;
+                        content = response.Content.ReadAsStringAsync().Result;
+                    }
+                }
+
+                var input = JsonConvert.DeserializeObject<Dictionary<string, object>>(content);
+
+                var start = DateTime.Now;
+                var sampledOutput = Sample(input);
+                var end = DateTime.Now;
+
+                var outputFormatting = Formatting.None;
+                if (indentOutput)
+                {
+                    outputFormatting = Formatting.Indented;
+                }
+
+                Console.WriteLine(JsonConvert.SerializeObject(sampledOutput, outputFormatting));
+
+                if (time)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Took { (end - start).TotalSeconds }s");
+                }
+
+                return (int)ExitCodes.Success;
             }
-
-            var content = File.ReadAllText(filePath);
-            var input = JsonConvert.DeserializeObject<Dictionary<string, object>>(content);
-
-            var start = DateTime.Now;
-            var sampledOutput = Sample(input);
-            var end = DateTime.Now;
-
-            var outputFormatting = Formatting.None;
-            if (indentOutput)
+            catch (Exception ex)
             {
-                outputFormatting = Formatting.Indented;
+                Console.Error.WriteLine(ex.Message);
+                return (int)ExitCodes.InternalError;
             }
-
-            Console.WriteLine(JsonConvert.SerializeObject(sampledOutput, outputFormatting));
-
-            if (time)
-            {
-                Console.WriteLine();
-                Console.WriteLine($"Took { (end - start).TotalSeconds }s");
-            }
-
-            return 0;
         }
 
         static Dictionary<string, object> Sample(Dictionary<string, object> input)
@@ -141,4 +167,12 @@ namespace jsam
             return result;
         }
     }
+
+    enum ExitCodes
+    {
+        Success = 0,
+        ImproperArguments,
+        InternalError,
+    }
 }
+
